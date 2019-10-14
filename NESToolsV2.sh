4088a6faae63eaa1a6959dac7099513ddc5eec13
BlaCkHat-NES <56505119+BlaCkHat-NES@users.noreply.github.com>
2019-10-14 21:24:39 +0700

    Add files via upload

---
+++ NESToolsV2.sh
@@ -0,0 +1,351 @@
+#!/system/xbin/bash
+# Bersihkan Layar
+#jangan direcode anjing
+#lu kira buat ini gampang
+#berusaha dong
+#gwe aja berdua usaha masa lu sendiri gak berusaha
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+read -p "masukkan user : " user
+read -p "masukkan pass : "
+
+if [ $user = "CHARLEz YT" ]|[ $pass = "NES" ]
+then
+echo "LOGIN SUKSES!"
+sleep 3
+sh NESToolsV2.sh
+else
+echo "USER & PASSWORD SALAH!"
+exit
+fi
+###################################################
+# Channel CHARLEz YT
+###################################################
+echo
+echo
+echo
+echo
+sleep 1
+figlet -f slant "SUBSCRIBE"|lolcat
+sleep 1
+figlet -f slant "CHARLEz YT"|lolcat
+sleep 3
+xdg-open 'https://www.youtube.com/channel/UCOAAa4uhKPdwantiHLD2TIA'
+clear
+toilet TOOLS NES|lolcat
+toilet 12 Tools|lolcat
+echo
+echo  $blue"╔══════════════════════════════════════════════════════════════════╗"
+echo  $purple"║ [ • ] TEAM      : NEWBIE ERROR SYSTEM                          ║"
+echo  $blue"║ [ • ] AUTHOR     : CHARLEz YT & DARK ICE                         ║"
+echo  $purple"║ [ • ] THANKS TO :CHARLEz YT & DARK ICE                         ║"
+echo  $blue"║ [ • ] :::::::::::::::::::::YOUTUBE CHANNEL:::::::::::::::::::::  ║"
+echo  $purple"║ [ • ] https://www.youtube.com/channel/UCOAAa4uhKPdwantiHLD2TIA ║"
+echo  $blue"╚══════════════════════════════════════════════════════════════════╝"
+echo  $yellow"╔═══════════════════════════════════════════╗"
+echo  $yellow"║ .:::::::::::::::::TOOLS:::::::::::::::::. ║"
+echo  $yellow"║ .::::::::::NEWBIE ERROR SYSTEM::::::::::. ║"
+echo  $yellow"╚═══════════════════════════════════════════╝"
+sleep 1
+echo  $blue" ═══════════════════════════════════════════════"
+echo  $red" [INSTALL] UNTUK MENGINSTALL PAKET"
+echo  $purple" ═══════════════════════════════════════════════"
+echo  $red" [00] EXIT"
+echo  $blue" ═══════════════════════════════════════════════"
+echo  $red" [01] VIRTEXT [UP DC] NEWBIE ERROR SYSTEM"
+echo  $purple" ═══════════════════════════════════════════════"
+echo  $red" [02] SPAM SMS BY zlucifer"
+echo  $blue" ═══════════════════════════════════════════════"
+echo  $red" [03] VIRTEXT BY  ⃘⃤ᴋɪʀᴀꙮ≈⊰0⊱"
+echo  $purple" ═══════════════════════════════════════════════"
+echo  $red" [04] DOWNLOAD APK SMS GRATIS"
+echo  $blue" ═══════════════════════════════════════════════"
+echo  $red" [05] CEK IP"
+echo  $purple" ═══════════════════════════════════════════════"
+echo  $red" [06] TEMBAK PAKET"
+echo  $blue" ═══════════════════════════════════════════════"
+echo  $red" [07] DDOS ATTACK (HAMMER)"
+echo  $purple" ═══════════════════════════════════════════════"
+echo  $red" [08] DOWNLOAD BUKU SAKTI HACKER"
+echo  $blue" ═══════════════════════════════════════════════"
+echo  $red" [09] INSTALL TOOLS TU4N BADUT(300TOOLS LEBIH)"
+echo  $purple" ═══════════════════════════════════════════════"
+echo  $red" [10] TOOLS SERBA GUNA"
+echo  $blue" ═══════════════════════════════════════════════"
+echo  $red" [11] CREATE VIRUS"
+echo  $purple" ═══════════════════════════════════════════════"
+echo  $red" [12] SEND VIRUS TROJAN"
+echo  $blue" ═══════════════════════════════════════════════"
+echo  $green""
+read -p "[CHARLEz YT]> " bro;
+
+if [ $bro = INSTALL ] || [ $bro = install ]
+then
+clear
+figlet -f slant "SANTUY"|lolcat
+echo
+echo
+sleep 1
+toilet PROSES | lolcat
+toilet INSTALLASI | lolcat
+sleep 3
+pkg install nmap
+pkg install python
+pkg install python2
+pkg install python3
+pip2 install requests
+pip2 install bs4
+pkg install lolcat
+pkg install toilet
+pip2 install mechanize
+pkg install cl
+pkg install vim
+gem install lolcat
+pkg install cowsay
+pkg install neofetch
+clear
+fi
+
+
+
+
+if [ $bro = 0 ] || [ $bro = 00 ]
+then
+clear
+figlet -f slant "SANTUY"|lolcat
+echo
+echo  $red" TERIMA KASIH TELAH MENGGUNAKAN TOOLS INI"
+sleep 3
+xdg-open 'https://www.youtube.com/channel/UCOAAa4uhKPdwantiHLD2TIA'
+clear
+exit
+fi
+
+if [ $bro = 1 ] || [ $bro = 01 ]
+then
+clear
+figlet -f slant "SANTUY"|lolcat
+sleep 3
+echo  $red"loading......"
+sleep 2
+echo  $red"ANDA AKAN DI ARAHKAN KE LINK DOWNLOAD NYA"
+sleep 5
+xdg-open 'https://semawur.com/ZFm2ZwTcv5'
+clear
+toilet TERIMA | lolcat
+toilet KASIH | lolcat
+fi
+
+if [ $bro = 2 ] || [ $bro = 02 ]
+then
+clear
+figlet -f slant "SANTUY"|lolcat
+sleep 2
+echo  $red" SPAM SMS BY zlucifer "
+sleep 1
+echo  $red" tunggu sebentar  "
+sleep 1
+echo  $red" loading..... "
+sleep 3
+pkg update && pkg upgrade
+pkg install git
+pkg install curl
+git clone https://github.com/zlucifer/troll_project
+cd troll_project
+chmod +×*
+ls
+bash troll.sh
+clear
+toilet TERIMA | lolcat
+toilet KASIH | lolcat
+fi
+
+if [ $bro = 3 ] || [ $bro = 03 ]
+then
+clear
+echo  $red"MOHON TUNGGU SEBENTAR"
+sleep 3
+echo  $red"ANDA AKAN DI ARAHKAN KE LINK DOWNLOAD"
+sleep 5
+xdg-open 'https://semawur.com/Z9FES'
+
+sleep 5
+clear
+toilet TERIMA | lolcat
+toilet KASIH | lolcat
+fi
+
+if [ $bro = 4 ] || [ $bro = 04 ]
+then
+clear
+echo  $green"SUBSCRIBE DLU CHANNEL CHARLEz YT"
+sleep 1
+echo  $green"GUE KASIH WAKTU 10 DETIK"
+xdg-open 'https://www.youtube.com/channel/UCOAAa4uhKPdwantiHLD2TIA'
+sleep 10
+echo  $red"MOHON TUNGGU SEBENTAR"
+sleep 3
+echo  $red"ANDA AKAN DI ARAHKAN KE LINK DOWNLOAD"
+sleep 1
+echo  $red"Loading......."
+sleep 3
+xdg-open 'https://semawur.com/nUlgRcSmbX8'
+sleep 5
+clear
+toilet TERIMA | lolcat
+toilet KASIH | lolcat
+fi
+
+
+
+
+if [ $bro = 5 ] || [ $bro = 05 ]
+then
+clear
+figlet -f slant "SANTUY"|lolcat
+sleep 2
+echo  $red" CEK IP "
+sleep 1
+echo  $red"SUBSCRIBE CHANNEL CHARLEz YT"
+sleep 3
+xdg-open 'https://www.youtube.com/channel/UCOAAa4uhKPdwantiHLD2TIA'
+pkg update
+pkg upgrade
+pkg install net-tools
+ifconfig
+
+sleep 3
+fi
+
+
+
+
+if [ $bro = 6 ] || [ $bro = 06 ]
+then
+clear
+figlet -f slant "SANTUY"|lolcat
+sleep 3
+echo  $red"loading......"
+sleep 2
+git clone https://github.com/bintang73/tembaktembakan
+cd tembaktembakan
+python dor.py
+
+clear
+toilet TERIMA | lolcat
+toilet KASIH | lolcat
+fi
+
+
+
+
+if [ $bro = 7 ] || [ $bro = 07 ]
+then
+clear
+figlet -f slant "SANTUY"|lolcat
+sleep 3
+echo  $red"loading......"
+sleep 2
+git clone https://github.com/cyweb/hammer
+cd hammer
+chmod +x hammer.py
+python hammer.py
+
+
+clear
+toilet TERIMA | lolcat
+toilet KASIH | lolcat
+fi
+
+
+
+
+
+if [ $bro = 8 ] || [ $bro = 08 ]
+then
+clear
+echo  $red" CREATOR : CHARLEz YT & DARK ICE"
+sleep 1
+figlet -f slant "SANTUY"|lolcat
+sleep 3
+echo  $red"loading......"
+sleep 2
+xdg-open 'https://semawur.com/U9JZ4ON12Fzz'
+
+clear
+toilet TERIMA | lolcat
+toilet KASIH | lolcat
+fi
+
+
+if [ $bro = 9 ] || [ $bro = 09 ]
+then
+clear
+figlet -f slant "SANTUY"|lolcat
+sleep 3
+echo  $red"loading......"
+sleep 2
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv3
+cd TOOLSINSTALLERv3
+chmod +x TUANB4DUT.sh
+sh TUANB4DUT.sh
+
+clear
+toilet TERIMA | lolcat
+toilet KASIH | lolcat
+fi
+
+
+if [ $bro = 10 ] || [ $bro = 010 ]
+then
+clear
+figlet -f slant "SANTUY"|lolcat
+sleep 3
+echo  $red"loading......"
+sleep 2
+git clone https://github.com/muhammadfathul/FBH
+cd FBH
+chmod +x*
+bash FBH
+clear
+toilet TERIMA | lolcat
+toilet KASIH | lolcat
+fi
+
+if [ $bro = 11 ] || [ $bro = 011 ]
+then
+clear
+figlet -f slant "SANTUY"|lolcat
+sleep 3
+echo  $red"loading......"
+sleep 2
+git clone https://github.com/LOoLzeC/Evil-create-framework
+cd Evil-create-framework
+python2 vcrt.py
+clear
+toilet TERIMA | lolcat
+toilet KASIH | lolcat
+fi
+
+
+if [ $bro = 12 ] || [ $bro = 012 ]
+then
+clear
+figlet -f slant "SANTUY"|lolcat
+sleep 3
+echo  $red"loading......"
+sleep 2
+git clone https://github.com/MrTamfanX/MrDdos
+cd MrDdos
+chmod +x*
+sh Tamfan-Ddos.sh
+clear
+toilet TERIMA | lolcat
+toilet KASIH | lolcat
+fi
